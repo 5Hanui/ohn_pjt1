6aa525fea18eb4b711a22fe840fe0ef4be1baa47
5Hanui <gkdud644@naver.com>
2020-06-07 17:34:05 +0900

    Dto, Dto Test 추가

--- src/main/java/com/ohn/book/springboot/web/HelloController.java
+++ src/main/java/com/ohn/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.ohn.book.springboot.web;
 
+import com.ohn.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // 컨트롤러를 JSON을 반환하는 컨트롤러 만들어줌
@@ -9,4 +11,11 @@ public class HelloController {
     public String hello(){
         return "hello";
     }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto( @RequestParam("name") String name,
+                                      @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
+
 }
--- src/test/java/com/ohn/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/ohn/book/springboot/web/HelloControllerTest.java
@@ -7,9 +7,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class) //테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자를 실행시킴
 @WebMvcTest(controllers = HelloController.class)
@@ -24,5 +24,20 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void returnHelloDto() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+        System.out.println("hello");
+        mvc.perform(get("/hello/dto")
+                   .param("name", name)
+                    .param("amount", String.valueOf(amount))) //param: 요청파라미터 설정, String값만 허용
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+                //jsonPath : JSON응답값을 필드별로 검증할 수 있는 메소드.
+                // $를 기준으로 필드명을 명시함.
     }
 }
